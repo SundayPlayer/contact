a254110a132102342cc443c2d3b8dfe52aa69f04
Hamid Kellali <hamid.kellali@gmail.com>
2016-12-21 14:41:42 +0100

    Fixed the methods

---
+++ src/ContactBundle/Entity/Category.php
@@ -0,0 +1,91 @@
+<?php
+namespace ContactBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table(name="category")
+ * @ORM\Entity(repositoryClass="ContactBundle\Repository\CategoryRepository")
+ */
+class Category
+{
+
+    /**
+     * @ORM\OneToMany(targetEntity="Contact", mappedBy="category")
+     */
+    private $contact;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get contact
+     *
+     * @return Contact
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+
+    /**
+     * Set contact
+     *
+     * @param Contact contact
+     *
+     * @return Category
+     */
+    public function setContact($contact)
+    {
+        $this->contact = $contact;
+    }
+}

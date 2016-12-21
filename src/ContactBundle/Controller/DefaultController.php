78b9e39151b83b2cc52f5b724122fc3d1c896d46
SundayPlayer <baptiste.vade@hotmail.fr>
2016-12-21 16:33:03 +0100

    Ajout du formulaire

--- src/ContactBundle/Controller/DefaultController.php
+++ src/ContactBundle/Controller/DefaultController.php
@@ -1,17 +1,22 @@
 <?php
 namespace ContactBundle\Controller;
 
+use ContactBundle\Entity\Contact;
+use ContactBundle\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
 
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
-    public function indexAction()
+    public function showHome()
     {
-        return $this->render('ContactBundle:Default:index.html.twig');
+        return $this->render('@Contact/Default/index.html.twig');
     }
+
+
 }
